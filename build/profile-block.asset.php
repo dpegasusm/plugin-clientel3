e433687841a49dfb267d6240ab9ccbb907625f10
David Marshall <dpegasusm@gmail.com>
2025-01-06 13:03:42 -0500

    NPM run build

--- build/profile-block.asset.php
+++ build/profile-block.asset.php
@@ -1,4 +1,1 @@
-<?php return array(
-	'dependencies' => array( 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives' ),
-	'version'      => 'ddc400e02cd03c218d3a',
-);
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-primitives'), 'version' => 'ddc400e02cd03c218d3a');
